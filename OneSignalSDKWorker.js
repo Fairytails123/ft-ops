14cb0e3d2338441d84a0c9d4717eb3343df8e8b1
Fairytails123 <manager@thefairytails.co.uk>
2026-03-18 06:02:00 +0000

    Create OneSignalSDKWorker.js

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,5 @@
+// OneSignalSDKWorker.js
+// Must be at the ROOT of your GitHub Pages repo
+// OneSignal automatically uses this file — no configuration needed here
+
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
